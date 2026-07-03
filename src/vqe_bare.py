af2dcda783190b362e30d6e60ecfab11a99b3bb1
Scirelgar <81189385+Scirelgar@users.noreply.github.com>
2026-07-03 14:25:27 -0400

    added annotation on vqe_bare.py

--- src/vqe_bare.py
+++ src/vqe_bare.py
@@ -22,7 +22,7 @@ def vqe_bare(input_hamiltonian: SparsePauliOp) -> OptimizeResult:
         >>> result = vqe_bare(Hamiltonian.H2_STO6G_REDUX.value)
     """
     # Define circuit and parameters objects
-    qc = QuantumCircuit(1)
+    qc = QuantumCircuit(1)  # 1 EXIT __init__() + 1 EXIT for arg
 
     params = [
         Parameter("theta"),
@@ -33,36 +33,44 @@ def vqe_bare(input_hamiltonian: SparsePauliOp) -> OptimizeResult:
     qc.rx(params[0], 0)
     qc.rz(params[1], 0)
 
+    qc.draw("mpl")  # PIN
+
     # Build the Hamiltonian
     hamiltonian = input_hamiltonian
 
     # Set up the estimator
-    estimator = StatevectorEstimator(seed=SEED)
+    estimator = StatevectorEstimator(seed=SEED)  # 1 EXIT __init__() + 1 EXIT arg
 
     # Define the cost function for optimization
-    def cost_function(
+    def cost_function(  # 1 Trig ENTRY + 4 ENTRIES for arg
         params, ansatz=qc, hamiltonian=hamiltonian, estimator=estimator
     ) -> float:
+
         pub_estimate = (
             ansatz,
             [hamiltonian],
             [params],
         )
-        result = estimator.run(
-            pubs=[pub_estimate],
-        ).result()
-        energy = result[0].data.evs[0]
-        return energy
+
+        # This is where quantum execution happens (could be extracted)
+        result = estimator.run(  # 1 EXIT method call QC
+            pubs=[pub_estimate],  # 1 EXIT tuple arg CL or QC?
+        ).result()  # 1 EXIT CL
+        energy = result[0].data.evs[0]  # PIN (HOW MANY ENTRIES)
+        return energy  # 1 EXIT to whoever called
 
     # Run the optimization
     initial_params = np.random.RandomState(seed=SEED).rand(len(params))
+    # 1 EXIT RandomState __init__(1 EXIT arg)
+    # 1 EXIT len( 1 EXIT arg)
+    # 1 EXIT rand()
 
-    optimization_result = minimize(
-        cost_function,
-        x0=initial_params,
-        args=(qc),
-        method="SLSQP",
-        options={"maxiter": 1000},
+    optimization_result = minimize(  # 1 EXIT
+        cost_function,  # 1 EXIT
+        x0=initial_params,  # 1 EXIT
+        args=(qc),  # 1 EXIT
+        method="SLSQP",  # 1 EXIT
+        options={"maxiter": 1000},  # 1 EXIT
     )
 
-    return optimization_result
+    return optimization_result  # 1 EXIT of vqe_bare()
