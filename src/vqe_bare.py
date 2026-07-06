02882282172a3fab922b327aefabc6dac7cb44cf
Scirelgar <samuel1438@hotmail.com>
2026-07-06 11:16:24 -0400

    updated annotation in vqe_bare

--- src/vqe_bare.py
+++ src/vqe_bare.py
@@ -7,8 +7,11 @@ from scipy.optimize import minimize, OptimizeResult
 
 SEED = 156
 
+"""
+FP vqe_bare
 
-def vqe_bare(input_hamiltonian: SparsePauliOp) -> OptimizeResult:
+"""
+def vqe_bare(input_hamiltonian: SparsePauliOp) -> OptimizeResult: # +1 Triggering Entry, +1 Entry for the argument
     """Run a VQE optimization using only the most basic Qiskit components.
 
     Args:
@@ -22,16 +25,41 @@ def vqe_bare(input_hamiltonian: SparsePauliOp) -> OptimizeResult:
         >>> result = vqe_bare(Hamiltonian.H2_STO6G_REDUX.value)
     """
     # Define circuit and parameters objects
-    qc = QuantumCircuit(1)  # 1 EXIT __init__() + 1 EXIT for arg
+    qc = QuantumCircuit(1)
+    """
+    FP vqe_bare (classical) : +1 Exit, +1 Exit for the parameter, +1 Entry for the object QuantumCircuit
+
+
+    FP Quantum Circuit (quantum) : +1 Triggering Entry, +1 Entry for the argument, +1 Exit for the object QuantumCircuit
+    
+    """
 
     params = [
         Parameter("theta"),
         Parameter("phi"),
     ]
+    """
+    FP vqe_bare (classical) : +2 Exit  Parameter (trigger+argument), +1 Entry  Parameter coming back
+
+    FP params (quantum) : +1 Triggering Entry coming from vqe_bare , +1 Entry for arguments , +1 Exits going back to vqe_bare 
+    
+    Total of exchange : 6
+    """
 
     # Build the ansatz
     qc.rx(params[0], 0)
     qc.rz(params[1], 0)
+   
+    """
+    FP vqe_bare (classical) : +2 Exits trig. each gates, +4 Exits for arguments
+
+    FP RX (quantum) : +2 Entries for arguments, +1 Triggering Entry, +1 Exit towards qc
+
+    FP RZ (quantum) : +2 Entries for argument, +1 Triggering Entry, +1 Exit towards qc
+
+    FP QuantumCircuit (quantum) : +2 Entries (one for each gates)
+    
+    """
 
     qc.draw("mpl")  # PIN
 
@@ -39,7 +67,13 @@ def vqe_bare(input_hamiltonian: SparsePauliOp) -> OptimizeResult:
     hamiltonian = input_hamiltonian
 
     # Set up the estimator
-    estimator = StatevectorEstimator(seed=SEED)  # 1 EXIT __init__() + 1 EXIT arg
+    estimator = StatevectorEstimator(seed=SEED) #__init__()
+
+    """
+    FP vqe_bare (classical) : +1 Exit (trigger), +1 Entry (object)
+
+    FP StatevectorEstimator (quantum) : +1 triggering Entry, +1 Exit (object)
+    """
 
     # Define the cost function for optimization
     def cost_function(  # 1 Trig ENTRY + 4 ENTRIES for arg
@@ -61,16 +95,26 @@ def vqe_bare(input_hamiltonian: SparsePauliOp) -> OptimizeResult:
 
     # Run the optimization
     initial_params = np.random.RandomState(seed=SEED).rand(len(params))
-    # 1 EXIT RandomState __init__(1 EXIT arg)
-    # 1 EXIT len( 1 EXIT arg)
-    # 1 EXIT rand()
+    """
+    FP vqe_bare : +1 Exit (trigger), +2 Exit for arguments, +2 Entries for values of parameters
 
-    optimization_result = minimize(  # 1 EXIT
-        cost_function,  # 1 EXIT
-        x0=initial_params,  # 1 EXIT
-        args=(qc),  # 1 EXIT
-        method="SLSQP",  # 1 EXIT
-        options={"maxiter": 1000},  # 1 EXIT
+    FP np.rand() (quantum) : +1 Triggering Entry, +2 Entries for arguments, +2 Exits for values of parameters
+
+    Total : 5
+    """
+
+    optimization_result = minimize(
+        cost_function, 
+        x0=initial_params, 
+        args=(qc), 
+        method="SLSQP", 
+        options={"maxiter": 1000}, 
     )
+    """
+    FP vqe_bare (classical) : +1 Exit (trigger), +5 Exits for arguments, +1 Entry for OptimizeResult
+
+    FP minimize (classical) : 
+    
+    """
 
     return optimization_result  # 1 EXIT of vqe_bare()
